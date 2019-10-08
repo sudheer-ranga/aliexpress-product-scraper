3b8719c1efb482d76da6bf8b14490239c8f238d3
Sudheer Ranga <sudheer.ranga1@gmail.com>
2019-10-08 19:49:28 +0530

    Create module for npm

--- index.js
+++ index.js
@@ -1,13 +1,2 @@
-const fs = require('fs');
-const productId = 32958933105;
-const scrape = require('./src/aliexpressProductScraper');
-const product = scrape(productId);
-
-product.then(res => {
-  console.log('The JSON: ', res);
-
-  fs.writeFile(`${productId}.json`, JSON.stringify(res), function(err) {
-    if (err) throw err;
-    console.log(`JSON reponse saved to ${productId}.json file!`);
-  });
-});
+const aliexpressProductScraper = require('./src/aliexpressProductScraper');
+module.exports = aliexpressProductScraper;
